7157385dde180b4c8026c5e3a777eec876864b30
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-07-09 10:44:37 +0600

    create many movies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+// Add a movie to the database
 async function createMovie() {
   const newMovie = await prisma.movie.create({
     data: {
@@ -14,8 +15,33 @@ async function createMovie() {
   });
 }
 
+// Add multiple movies to the database
+async function createMultipleMovies() {
+  const movies = await prisma.movie.createMany({
+    data: [
+      {
+        title: "The Dark Knight",
+        description:
+          "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
+        genre: "Action",
+        releaseDate: new Date("2008-07-18"),
+        rating: 9.0,
+      },
+      {
+        title: "Alien Covenant",
+        description:
+          "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        genre: "Adventure",
+        releaseDate: new Date("2014-11-07"),
+        rating: 8.6,
+      },
+    ],
+  });
+}
+
 async function main() {
   await createMovie();
+  await createMultipleMovies();
 }
 
 main()
